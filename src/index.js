a535de65a52aa96e901746b7c0565cb5da38ac86
Vlad <vlad09fedko@gmaul.com>
2026-06-07 12:06:29 +0300

    Make the function non-multitasking

--- src/index.js
+++ src/index.js
@@ -6,47 +6,57 @@ const cityIdInput = document.querySelector('#city-id');
 const radioInputs = document.querySelectorAll('input[type=radio]');
 const infoFields = document.querySelectorAll('.info');
 
+function doUrl() {
+  if (!cityNameInput.value.trim() && !cityIdInput.value.trim()) {
+    throw new Error('Void! One of the fields must be filled in!');
+  }
+
+  const API_KEY = '09a3c4c45fbe46453b0a970485b97374';
+  let data;
+
+  if (!cityNameInput.hasAttribute('disabled')) {
+    const cityName = cityNameInput.value.trim();
+    data = `q=${cityName}`;
+  } else if (!cityIdInput.hasAttribute('disabled')) {
+    const cityId = cityIdInput.value.trim();
+    data = `id=${cityId}`;
+  }
+
+  return `https://api.openweathermap.org/data/2.5/weather?${data}&appid=${API_KEY}&units=metric`;
+}
+
+async function getData(url) {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error(res.statusText);
+  return res;
+}
+
+function fillOutFields(temp, windSpeed, humidity) {
+  infoFields.forEach(field => {
+    field.classList.remove('error-text');
+    if (field.id === 'temp') {
+      field.textContent = `${temp}° C`;
+    }
+    if (field.id === 'wind-speed') {
+      field.textContent = `${windSpeed} m/s`;
+    }
+    if (field.id === 'humidity') {
+      field.textContent = `${humidity}%`;
+    }
+  });
+}
+
+function updateInputs() {
+  function toggleInput(inp) {
+    inp.toggleAttribute('disabled');
+    inp.classList.toggle('disabled');
+  }
+
+  toggleInput(cityNameInput);
+  toggleInput(cityIdInput);
+}
+
 async function getWeather(e) {
-  function doUrl() {
-    if (!cityNameInput.value.trim() && !cityIdInput.value.trim()) {
-      throw new Error('Void! One of the fields must be filled in!');
-    }
-
-    const API_KEY = '09a3c4c45fbe46453b0a970485b97374';
-    let data;
-
-    if (!cityNameInput.hasAttribute('disabled')) {
-      const cityName = cityNameInput.value.trim();
-      data = `q=${cityName}`;
-    } else if (!cityIdInput.hasAttribute('disabled')) {
-      const cityId = cityIdInput.value.trim();
-      data = `id=${cityId}`;
-    }
-
-    return `https://api.openweathermap.org/data/2.5/weather?${data}&appid=${API_KEY}&units=metric`;
-  }
-
-  async function getData(url) {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error(res.statusText);
-    return res;
-  }
-
-  function fillOutFields(temp, windSpeed, humidity) {
-    infoFields.forEach(field => {
-      field.classList.remove('error-text');
-      if (field.id === 'temp') {
-        field.textContent = `${temp}° C`;
-      }
-      if (field.id === 'wind-speed') {
-        field.textContent = `${windSpeed} m/s`;
-      }
-      if (field.id === 'humidity') {
-        field.textContent = `${humidity}%`;
-      }
-    });
-  }
-
   try {
     e.preventDefault();
     const url = doUrl();
@@ -68,16 +78,6 @@ async function getWeather(e) {
     });
     console.error(err.message);
   }
-}
-
-function updateInputs() {
-  function toggleInput(inp) {
-    inp.toggleAttribute('disabled');
-    inp.classList.toggle('disabled');
-  }
-
-  toggleInput(cityNameInput);
-  toggleInput(cityIdInput);
 }
 
 radioInputs.forEach(radioInput => {
